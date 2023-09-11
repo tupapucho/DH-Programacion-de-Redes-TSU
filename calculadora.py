6545411640b8b5c28c77cf7b795427442f65f1ac
tupapucho <tonyu705@gmail.com>
2023-09-11 12:56:22 -0500

    ggez

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+'''
+autor: Antonio Uribe Ramirez
+fecha: 11 septiembre 2023
+descripcion: calculadora 
+'''
+
+a = input ("ingrese un valor")
+b = input ("ingresa un valor")
+
+print (type (a) , type(b)
+
+a = int ()
