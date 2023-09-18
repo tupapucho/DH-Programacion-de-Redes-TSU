cb3bdfc70ba66f29c306a1c256406125eca6ea89
Antonio Uribe <142758156+tupapucho@users.noreply.github.com>
2023-09-18 11:39:44 -0600

    Add files via upload

---
+++ Ejercios de laboratorio/lab 2.1.1.18.py
@@ -0,0 +1,7 @@
+'''
+Nom: Antonio Uribe 
+ejercico: lab 18
+'''
+
+print( "Fundamentos","Programación","en",sep="***",end="...")
+print("Python")
---
+++ Ejercios de laboratorio/lab 2.1.1.19.py
@@ -0,0 +1,41 @@
+'''
+Nomb:Antonio Uribe 
+Actividad: lab 19
+'''
+
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+print("        *","\n       *#*","\n      *###*","\n     *#####*","\n    *#######*","\n      *###*","\n      *###*","\n      *****\n")
+
+print("    *     "*2)
+print("   * *    "*2)
+print("  *   *   "*2)
+print(" *     *  "*2)
+print("***   *** "*2)
+print("  *   *   "*2)
+print("  *   *   "*2)
+print("  *****   "*2)
+
+print("    *")
+print("    *")
+print("   * *")
+print("   * *")
+print("  *   *")
+print("  *   *")
+print(" *     *")
+print(" *     *")
+print("***   ***")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *   *")
+print("  *   *")
+print("  *****")
+print("  *****")
