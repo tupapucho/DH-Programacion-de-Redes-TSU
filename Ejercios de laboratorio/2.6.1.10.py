e4594dd7e7f825cb532906d7c5a18c40d2da9b74
Antonio Uribe <142758156+tupapucho@users.noreply.github.com>
2023-10-02 09:39:21 -0600

    Add files via upload

---
+++ Ejercios de laboratorio/2.6.1.10.py
@@ -0,0 +1,8 @@
+'''
+Nombre: Antonio Uribe Ramirez 
+Fecha: 02/20/23
+practica: lab 2.6.1.10
+'''
+x = float(input("Ingrese el valor de x: "))
+y = 1.0 / (x + 1.0 / (x + 1.0 / (x + 1.0 / x)))
+print("y es:", y)
