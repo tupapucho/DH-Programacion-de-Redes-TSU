77de0c37f25086860109c8abddf661f12ad9c0c8
Antonio Uribe <142758156+tupapucho@users.noreply.github.com>
2023-10-02 10:00:01 -0600

    Add files via upload

---
+++ Ejercios de laboratorio/lab 2.5.1.2.py
@@ -0,0 +1,22 @@
+'''
+Nombre: Antonio Uribe Ramirez
+Practica: Laboratorio 2.5.1.2
+Fecha: 25/09/23
+'''
+
+# este programa calcula los segundos en cierto número de horas determinadas 
+# este programa fue escrito hace dos días
+# Autor [introuzca su nombre]
+
+# Definimos el número de horas
+a = 2 
+
+# Número de segundos en una hora
+seconds = 3600 
+
+
+print("Horas: ", a) #imprime el numero de horas
+print("Segundos en Horas: ", a * seconds) # se imprime el numero de segundos en determinado numero de horas
+
+#aquí también se debe de imprimir un "Adiós", pero el programador no tuvo tiempo de escribirlo
+#este el es fin del programa que calcula el numero de segundos en 2 horas
