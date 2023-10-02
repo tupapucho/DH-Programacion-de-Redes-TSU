6a06a8ebce5f3fef9063e909353df909bc40720f
Antonio Uribe <142758156+tupapucho@users.noreply.github.com>
2023-10-02 09:58:15 -0600

    Add files via upload

---
+++ Ejercios de laboratorio/lab 2.4.1.7.py
@@ -0,0 +1,15 @@
+'''
+Nombre: Antonio Uribe Ramirez
+Practica: Laboratorio 2.4.1.7
+Fecha: 25/09/23
+'''
+
+Juan = 3
+Maria = 5
+Adan = 6
+
+print(Juan, Maria, Adan)
+
+total_manzanas = Juan + Maria + Adan
+
+print("Número Total de Manzanas:", total_manzanas)
