1c768dbfccaba210578ae1fb4bcf4c9d431ef8e2
Antonio Uribe <142758156+tupapucho@users.noreply.github.com>
2023-10-02 10:05:15 -0600

    Add files via upload

---
+++ Ejercios de laboratorio/lab 2.4.1.10.py
@@ -0,0 +1,20 @@
+'''
+Nombre: Antonio Uribe Ramirez
+Practica: Laboratorio 2.4.1.10
+Fecha: 25/09/23
+'''
+
+x = 0
+x = float (x)
+y = (3 * x**3) - (2 * x**2) + (3 * x) - 1
+print("y =", y)
+
+x = 1
+x = float (x)
+y = (3 * x**3) - (2 * x**2) + (3 * x) - 1
+print("y =", y)
+
+x = -1
+x = float (x)
+y = (3 * x**3) - (2 * x**2) + (3 * x) - 1
+print("y =", y)
