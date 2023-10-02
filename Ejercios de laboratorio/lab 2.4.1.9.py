26b70c8ec25f9161f2b1a1398ad22659e429f001
Antonio Uribe <142758156+tupapucho@users.noreply.github.com>
2023-10-02 09:59:15 -0600

    Add files via upload

---
+++ Ejercios de laboratorio/lab 2.4.1.9.py
@@ -0,0 +1,14 @@
+'''
+Nombre: Antonio Uribe Ramirez
+Practica: Laboratorio 2.4.1.9
+Fecha: 25/09/23
+'''
+
+kilometers = 12.25
+miles = 7.38
+
+miles_to_kilometers = miles * 1.61
+kilometers_to_miles = kilometers / 1.61
+
+print(miles, "millas son", round(miles_to_kilometers, 2), "kilómetros")
+print(kilometers, "kilómetros son", round(kilometers_to_miles, 2), "millas")
