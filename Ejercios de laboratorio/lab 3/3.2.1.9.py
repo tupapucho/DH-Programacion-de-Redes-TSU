10d80bc14e18f68a47b0721710c066516f3772cc
Antonio Uribe <142758156+tupapucho@users.noreply.github.com>
2023-10-16 23:26:38 -0600

    Add files via upload

---
+++ Ejercios de laboratorio/lab 3/3.2.1.9.py
@@ -0,0 +1,10 @@
+'''
+Antonio Uribe 
+Lab 3.2.1.9
+16/10/23
+'''
+while True:
+    palabra = input("Ingresa una palabra: ")
+    if palabra == "chupacabra":
+        print("¡Has dejado el bucle con éxito!")
+        break
